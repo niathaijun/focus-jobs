cc52c9697a43e043e315c9c111e14b1f1f8ffaf6
langweibo <niathaijun2016@gmail.com>
2017-05-14 00:03:11 +0800

    implement function publish/hidden

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,6 +3,10 @@ class JobsController < ApplicationController
 
   def show
     @job = Job.find(params[:id])
+    if @job.is_hidden
+       flash[:warning] = "此职位已归档！"
+       redirect_to root_path
+     end
   end
 
   def index
